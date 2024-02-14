e05ef49ecde8fe85a16483ac3f70e4378a01d2a2
itskharina <137315976+itskharina@users.noreply.github.com>
2024-02-14 22:54:48 +0000

    Added API functionality with LeafletJS

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+const ip = document.querySelector('.ip-data');
+const place = document.querySelector('.location-data');
+const timezone = document.querySelector('.timezone-data');
+const isp = document.querySelector('.isp-data');
+let long;
+let lat;
+
+const locationIcon = L.icon({
+  iconUrl: 'images/icon-location.svg',
+  iconSize: [30, 40],
+  iconAnchor: [15, 40],
+});
+
+window.onload = async function () {
+  // const data = await fetchApi('8.8.8.8');
+  const data = await fetchApi();
+  renderData(data);
+};
+
 async function fetchApi() {
   try {
-    const response = await fetch('https://ipapi.co/8.8.8.8/json/');
+    const response = await fetch(`https://ipapi.co/json/`);
     const data = await response.json();
     console.log(data);
     return data;
@@ -9,23 +28,19 @@ async function fetchApi() {
   }
 }
 
-fetchApi();
+function renderData(data) {
+  ip.textContent = data.ip;
+  place.textContent = `${data.city}, ${data.country_name}`;
+  timezone.textContent = `UTC ${data.utc_offset}`;
+  isp.textContent = data.org;
+  lat = data.latitude;
+  long = data.longitude;
 
-// function renderData() {}
-
-const map = L.map('map').setView([37.42301, -122.083352], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-const locationIcon = L.icon({
-  iconUrl: 'images/icon-location.svg',
-  iconSize: [30, 40],
-  iconAnchor: [15, 40],
-});
-
-const marker = L.marker([37.42301, -122.083352], { icon: locationIcon }).addTo(
-  map
-);
+  let map = L.map('map').setView([lat, long], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  let marker = L.marker([lat, long], { icon: locationIcon }).addTo(map);
+}
