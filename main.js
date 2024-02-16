457b679e0c061936e685291b34f1f876cc572260
itskharina <137315976+itskharina@users.noreply.github.com>
2024-02-16 03:04:25 +0000

    Added search IP functionality

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const ip = document.querySelector('.ip-data');
 const place = document.querySelector('.location-data');
 const timezone = document.querySelector('.timezone-data');
 const isp = document.querySelector('.isp-data');
+const button = document.querySelector('button');
+const search = document.querySelector('input');
+
 let long;
 let lat;
+let map;
+let marker;
 
 const locationIcon = L.icon({
   iconUrl: 'images/icon-location.svg',
@@ -12,16 +17,31 @@ const locationIcon = L.icon({
 });
 
 window.onload = async function () {
-  // const data = await fetchApi('8.8.8.8');
   const data = await fetchApi();
   renderData(data);
+  map = L.map('map').setView([lat, long], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  marker = L.marker([lat, long], { icon: locationIcon }).addTo(map);
 };
 
 async function fetchApi() {
   try {
-    const response = await fetch(`https://ipapi.co/json/`);
+    const response = await fetch('https://ipapi.co/json/');
     const data = await response.json();
-    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchApiInput(input) {
+  try {
+    const response = await fetch(`https://ipapi.co/${input}/json/`);
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
@@ -35,12 +55,12 @@ function renderData(data) {
   isp.textContent = data.org;
   lat = data.latitude;
   long = data.longitude;
-
-  let map = L.map('map').setView([lat, long], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-  let marker = L.marker([lat, long], { icon: locationIcon }).addTo(map);
 }
+
+button.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const data = await fetchApiInput(search.value);
+  renderData(data);
+  map.setView([lat, long], 13);
+  marker.setLatLng([lat, long]);
+});
